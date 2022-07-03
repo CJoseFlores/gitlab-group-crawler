6f4730ff9f268dc056c11f2534916867b55fc4e5
Carlos Flores <cjflores96@outlook.com>
2022-07-03 12:17:15 -0400

    Added more cmd options

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
 	app := &cli.App{
 		Name:  "gitlab-project-name-crawler",
 		Usage: "Prints out the names of projects underneath a GitLab group (recursively)",
+		UsageText: "[global options] command [command options] [arguments...]" +
+			"\n\nThe list of arguments passed in are gitlab groups or subgroups you wish to scan recursively",
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:        "insecure",
@@ -22,11 +24,36 @@ func main() {
 				Aliases:     []string{"k"},
 				Destination: &args.Insecure,
 			},
+			&cli.StringFlag{
+				Name:        "gitlab-url",
+				Value:       "https://gitlab.com/",
+				Usage:       "the URL of the gitlab instance to crawl",
+				Aliases:     []string{"g"},
+				Destination: &args.GitlabUrl,
+			},
+			&cli.StringFlag{
+				Name:        "file-name",
+				Usage:       "the name of an existing file to diff against",
+				Aliases:     []string{"f"},
+				Destination: &args.InputFileName,
+			},
+			&cli.StringFlag{
+				Name:        "output-file-name",
+				Value:       "project-list.txt",
+				Usage:       "the name of the output file to generate",
+				Aliases:     []string{"o"},
+				Destination: &args.OutputFileName,
+			},
 		},
 		Action: func(ctx *cli.Context) error {
+			if firstArg := ctx.Args().First(); firstArg != "" {
+				fmt.Println("Argument: " + firstArg)
+			}
+
 			fmt.Println("Unimplemented")
 			return nil
 		},
+		EnableBashCompletion: true,
 	}
 
 	if err := app.Run(os.Args); err != nil {
